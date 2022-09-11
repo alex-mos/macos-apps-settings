82258fa129c23776c2334fb4dea7ca131dc9d841
Aleksandr Mospan <alexander.mospan@gmail.com>
2022-09-11 21:02:47 +0200

    update install script

--- install.sh
+++ install.sh
@@ -1,3 +1,14 @@
 # disable apple music launch with play button
 launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist
 
+# karabiner
+rm -rf ~/.config/karabiner
+ln -s ~/settings/karabiner ~/.config/karabiner
+
+# iterm
+
+# alt-tab
+
+# sublime text
+
+# webstorm
