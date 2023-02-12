a25d72ea0482083cd5518a1ab5f1c949d5b83170
Aleksandr Mospan <alexander.mospan@gmail.com>
2023-02-12 23:59:39 +0100

    upd install script

--- install.sh
+++ install.sh
@@ -1,3 +1,32 @@
+brew tap "crowdin/crowdin"
+brew install ffmpeg
+brew install p7zip
+brew install pipenv
+brew install yt-dlp
+brew install zola
+brew install crowdin/crowdin/crowdin@3
+brew install --cask alt-tab
+brew install --cask dropbox
+brew install --cask firefox
+brew install --cask freedom
+brew install --cask karabiner-elements
+brew install --cask michaelvillar-timer
+brew install --cask netnewswire
+brew install --cask notion
+brew install --cask numi
+brew install --cask obsidian
+brew install --cask sublime-merge
+brew install --cask sublime-text
+brew install --cask syncthing
+brew install --cask telegram
+brew install --cask ukelele
+brew install --cask unnaturalscrollwheels
+brew install --cask vlc
+brew install --cask webstorm
+
+# clone repository with settings
+git clone git@github.com:alex-mos/macos-apps-settings.git ~/settings
+
 # disable apple music launch with play button
 launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist
 
@@ -6,10 +35,11 @@ rm -rf ~/.config/karabiner
 ln -s ~/settings/karabiner ~/.config/karabiner
 
 # iterm
+# configure according to the screenshot iterm-settings.png
 
 # alt-tab
-# rm -rf ~/Library/Preferences/com.lwouis.alt-tab-macos.plist
-# ln -s ~/settings/com.lwouis.alt-tab-macos.plist ~/Library/Preferences/com.lwouis.alt-tab-macos.plist
+# configure according to the screenshots in alt-tab folder
+
 # sublime text
 
 # webstorm
