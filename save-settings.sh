298350b4cd4b046319c1aeedc6e02817ad61f0b7
Aleksandr Mospan <alexander.mospan@gmail.com>
2023-09-30 20:30:58 +0200

    add scripts

--- save-settings.sh
+++ save-settings.sh
@@ -1,29 +1,34 @@
-# alt-tab
+# applications settings
+
+## alt-tab
 mkdir -p alt-tab/library/preferences/
 cp -R ~/Library/Preferences/com.lwouis.alt-tab-macos.plist alt-tab/Library/Preferences/ alt-tab/library/preferences/
 
-# iterm2
+## iterm2
 mkdir -p iterm2/library/preferences/
 cp -R ~/Library/Preferences/com.googlecode.iterm2.plist iterm2/Library/Preferences/ iterm2/library/preferences/
 
-# zsh
+## zsh
 mkdir -p zsh
 cp ~/.zshrc zsh
 
-# Karabiner Elements
+## Karabiner Elements
 mkdir -p karabiner/config/
 cp ~/.config/karabiner/karabiner.json karabiner/config/
 
-# Sublime Text
+## Sublime Text
 mkdir -p sublime-text/library/application-support/packages/user
 mkdir -p sublime-text/library/application-support/installed-packages/
 cp -R ~/Library/Application\ Support/Sublime\ Text/Packages/User/ sublime-text/library/application-support/packages/user/
 cp -R ~/Library/Application\ Support/Sublime\ Text/Installed\ Packages/ sublime-text/library/application-support/installed-packages/
 
-# Sublime Merge
+## Sublime Merge
 mkdir -p sublime-merge/library/application-support/packages/user
 cp -R ~/Library/Application\ Support/Sublime\ Merge/Packages/User/ sublime-merge/library/application-support/packages/user/
 
-# Rectangle
+## Rectangle
 mkdir -p rectangle/library/preferences/
 cp -R ~/Library/Preferences/com.knollsoft.Rectangle.plist rectangle/library/preferences/
+
+# scripts
+cp -R ~/scripts scripts
